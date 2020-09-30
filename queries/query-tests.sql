e40ea014aae11e4f9ccd51857d4de10f4c0b7157
randmark <47195678+randmark@users.noreply.github.com>
2020-09-30 11:02:53 -0500

    Update query-tests.sql

--- queries/query-tests.sql
+++ queries/query-tests.sql
@@ -6,12 +6,10 @@ select * from postgres_laptop.public.lineitem ;
 
 select * from aws_rds_postgres.public.orders ;
 
-select count(1) from aws_rds_postgres.public.orders;
-
 select su.name, li.orderkey, ord.clerk from 
 	postgres_laptop.public.supplier su 
 	inner join postgres_laptop.public.lineitem li on su.suppkey = li.suppkey 
-	inner join aws_rds_postgres.public.orders ord on ord.orderkey = li.orderkey limit 100 ;
+	inner join aws_rds_postgres.public.orders ord on ord.orderkey = li.orderkey;
 
 create table aws_rds_postgres.public.supplier as
 select * from postgres_laptop.public.supplier ;

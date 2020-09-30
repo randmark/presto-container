7b05ffa4d3022049a4acc93afbef21f47444ad6c
randmark <47195678+randmark@users.noreply.github.com>
2020-09-29 19:28:43 -0500

    Change docker run from DIR

--- demo-container-latest.sh
+++ demo-container-latest.sh
@@ -2,6 +2,10 @@
 
 docker rm presto-demo-latest
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+echo $DIR
+
 # docker run \
 # -v ~/containers/presto/etc/catalog:/usr/lib/presto/etc/catalog \
 # -v ~/containers/presto/log:/var/log/presto \
@@ -10,6 +14,17 @@ docker rm presto-demo-latest
 # -v ~/containers/presto/etc/log.properties:/usr/lib/presto/etc/log.properties \
 # -v ~/containers/presto/etc/node.properties:/usr/lib/presto/etc/node.properties \
 # -p 8080:8080 --name presto-demo-latest starburstdata/presto
+
+
+docker run \
+-v $DIR/etc/catalog:/usr/lib/presto/etc/catalog \
+-v $DIR/log:/var/log/presto \
+-v $DIR/etc/config.properties:/usr/lib/presto/etc/presto/config.properties \
+-v $DIR/etc/jvm.config:/usr/lib/presto/etc/jvm.config \
+-v $DIR/etc/log.properties:/usr/lib/presto/etc/log.properties \
+-v $DIR/etc/node.properties:/usr/lib/presto/etc/node.properties \
+-p 8080:8080 --name presto-demo-latest starburstdata/presto
+
 
 
 docker run -p 8080:8080 --name presto-demo-latest starburstdata/presto

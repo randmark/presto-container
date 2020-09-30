81f71a97fbf96613f9846c77d5937e0c46322bec
randmark <47195678+randmark@users.noreply.github.com>
2020-09-29 20:13:32 -0500

    Updating to properties that work for everyone.

--- demo-container-latest.sh
+++ demo-container-latest.sh
@@ -1,20 +1,10 @@
 #!/usr/bin/env bash
 
-docker rm presto-demo-latest
+#docker rm presto-demo-latest
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 echo $DIR
-
-# docker run \
-# -v ~/containers/presto/etc/catalog:/usr/lib/presto/etc/catalog \
-# -v ~/containers/presto/log:/var/log/presto \
-# -v ~/containers/presto/etc/config.properties:/usr/lib/presto/etc/presto/config.properties \
-# -v ~/containers/presto/etc/jvm.config:/usr/lib/presto/etc/jvm.config \
-# -v ~/containers/presto/etc/log.properties:/usr/lib/presto/etc/log.properties \
-# -v ~/containers/presto/etc/node.properties:/usr/lib/presto/etc/node.properties \
-# -p 8080:8080 --name presto-demo-latest starburstdata/presto
-
 
 docker run \
 -v $DIR/etc/catalog:/usr/lib/presto/etc/catalog \
@@ -23,8 +13,5 @@ docker run \
 -v $DIR/etc/jvm.config:/usr/lib/presto/etc/jvm.config \
 -v $DIR/etc/log.properties:/usr/lib/presto/etc/log.properties \
 -v $DIR/etc/node.properties:/usr/lib/presto/etc/node.properties \
+-d --rm \
 -p 8080:8080 --name presto-demo-latest starburstdata/presto
-
-
-
-docker run -p 8080:8080 --name presto-demo-latest starburstdata/presto

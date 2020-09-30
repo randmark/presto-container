20c1b8c4c4fcc1e47c550cfafe5e16d2c3d5429d
randmark <47195678+randmark@users.noreply.github.com>
2020-09-29 22:31:16 -0500

    Updated sql files

--- queries/query-tests.sql
+++ queries/query-tests.sql
@@ -28,8 +28,20 @@ select * from postgres_laptop.public
 select count(1) from tpch.sf1.customer ;
 
 create table postgres_laptop.public.lineitem as
-select * from tpch.sf1.lineitem ;
+select * from tpch.tiny.lineitem ;
+
+create table postgres_laptop.public.supplier as
+select * from tpch.tiny.supplier ;
 
 select * from postgres_laptop.public.supplier ;
 
-select * from aws_rds_postgres.public.orders ord inner join postgres_laptop.
+
+select * from postgres_laptop.public.supplier ;
+
+select * from postgres_laptop.public.lineitem ;
+
+select su.name, li.orderkey, ord.clerk from 
+	postgres_laptop.public.supplier su 
+	inner join postgres_laptop.public.lineitem li on su.suppkey = li.suppkey 
+	inner join aws_rds_postgres.public.orders ord on ord.orderkey = li.orderkey ;
+

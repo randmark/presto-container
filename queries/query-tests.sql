e418e0ba5d2128247e0d98fffbd542cf49145b55
randmark <47195678+randmark@users.noreply.github.com>
2020-09-30 10:18:55 -0500

    Updated the queries.

--- queries/query-tests.sql
+++ queries/query-tests.sql
@@ -6,11 +6,14 @@ select * from postgres_laptop.public.lineitem ;
 
 select * from aws_rds_postgres.public.orders ;
 
+select count(1) from aws_rds_postgres.public.orders;
+
 select su.name, li.orderkey, ord.clerk from 
 	postgres_laptop.public.supplier su 
 	inner join postgres_laptop.public.lineitem li on su.suppkey = li.suppkey 
-	inner join aws_rds_postgres.public.orders ord on ord.orderkey = li.orderkey ;
+	inner join aws_rds_postgres.public.orders ord on ord.orderkey = li.orderkey limit 100 ;
 
-create table postgres_laptop.public.partsupp as
-select * from tpch.tiny.partsupp ;
+create table aws_rds_postgres.public.supplier as
+select * from postgres_laptop.public.supplier ;
+
 

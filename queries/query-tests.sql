08cd232532e2e599758592eaeda3d024adc25a72
randmark <47195678+randmark@users.noreply.github.com>
2021-06-21 11:21:58 -0500

    Checking in removing the properties file

--- queries/query-tests.sql
+++ queries/query-tests.sql
@@ -11,7 +11,13 @@ select su.name, li.orderkey, ord.clerk from
 	inner join postgres_laptop.public.lineitem li on su.suppkey = li.suppkey 
 	inner join aws_rds_postgres.public.orders ord on ord.orderkey = li.orderkey;
 
+select * from aws_rds_postgres.public.orders ;
+
+select * from aws_rds_postgres.public.
+
 create table aws_rds_postgres.public.supplier as
 select * from postgres_laptop.public.supplier ;
+
+
 
 
